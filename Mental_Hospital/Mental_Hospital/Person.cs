eedf18125731da77eb112fbcd20056d865e92233
Yan-Pavel Hantsavich <s26822@pjwstk.edu.pl>
2024-10-20 20:15:27 +0200

    Created the solution with projects for class storage and testing. Implemented Person.cs class.

---
+++ Mental_Hospital/Mental_Hospital/Person.cs
@@ -0,0 +1,14 @@
+﻿namespace Mental_Hospital;
+
+public abstract class Person
+{
+    private string _name{ get; set; }
+    private string _surname{ get; set; }
+    private DateTime _dateOfBirth{ get; set; }
+    private string _address{ get; set; }
+
+    public Person()
+    {
+        
+    }
+}
---
+++ Mental_Hospital/Mental_Hospital_Tests/UnitTest1.cs
@@ -0,0 +1,17 @@
+using Mental_Hospital;
+
+namespace Mental_Hospital_Tests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}

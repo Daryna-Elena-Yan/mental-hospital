dc756dbc85ff7e484230bfe73ed8b7dd97dc6089
Daryna Vlasiuk <s26878@pjwstk.edu.pl>
2024-10-22 16:39:43 +0200

    Added new classes' initializations to ServiceCollectionExtensions.cs

--- Mental_Hospital/Mental_Hospital/ServiceCollectionExtensions.cs
+++ Mental_Hospital/Mental_Hospital/ServiceCollectionExtensions.cs
@@ -10,9 +10,26 @@ public static class ServiceCollectionExtensions
     public static ServiceCollection MentalHospitalSetup(this ServiceCollection serviceCollection)
     {
         //part of innitializing
-        serviceCollection.AddSingleton<Storage<Person>>();
         serviceCollection.AddTransient<Patient>();
-        serviceCollection.AddTransient<PersonFactory>();
+        serviceCollection.AddSingleton<Storage<Person>>();
+        serviceCollection.AddSingleton<PersonFactory>();
+        
+        serviceCollection.AddTransient<Appointment>();
+        serviceCollection.AddSingleton<Storage<Appointment>>();
+        serviceCollection.AddSingleton<AppointmentFactory>();
+        
+        serviceCollection.AddTransient<Prescription>();
+        serviceCollection.AddSingleton<Storage<Prescription>>();
+        serviceCollection.AddSingleton<PrescriptionFactory>();
+        
+        serviceCollection.AddTransient<Room>();
+        serviceCollection.AddSingleton<Storage<Room>>();
+        serviceCollection.AddSingleton<RoomFactory>();
+        
+        serviceCollection.AddTransient<Equipment>();
+        serviceCollection.AddSingleton<Storage<Equipment>>();
+        serviceCollection.AddSingleton<EquipmentFactory>();
+        
         return serviceCollection;
     }
 }

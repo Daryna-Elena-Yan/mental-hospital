3365fa85da99f425eeb9d7ec6de2ec37fcd34adb
Yan-Pavel Hantsavich <s26822@pjwstk.edu.pl>
2024-10-24 01:27:46 +0200

    Added TherapistStorageActions.cs. Made some comments for future consideration. Added collections to Patient.cs and Therapist.cs.

--- Mental_Hospital/Mental_Hospital/Models/Therapist.cs
+++ Mental_Hospital/Mental_Hospital/Models/Therapist.cs
@@ -6,4 +6,5 @@ public class Therapist:Employee
     public static double OvertimePaidPerHourInZl=70;
     public List<string> Qualifications { get; set; }
     public virtual ICollection<Appointment> Appointments { get; } = [];
+    public virtual ICollection<Patient> Patients{ get; }= new List<Patient>();
 }
---
+++ Mental_Hospital/Mental_Hospital/Storages/TherapistStorageActions.cs
@@ -0,0 +1,28 @@
+﻿using Mental_Hospital.Models;
+
+namespace Mental_Hospital.Storages;
+
+public class TherapistStorageActions:IStorageAction<Therapist>
+{
+    private readonly Storage<Appointment> _approintmentStorage;
+    private readonly Storage<Prescription> _prescriptionStorage;
+    public void OnDelete(Therapist item)
+    {
+        foreach (var appointment in item.Appointments.ToList())
+        {
+            for (int i = 0; i < appointment.Prescriptions.Count; i++)
+            {
+                _prescriptionStorage.Delete(appointment.Prescriptions[i]);//////////////////////////////////////////////////////
+            }
+            _approintmentStorage.Delete(appointment);
+        }
+        foreach (var patient in item.Patients)
+        {
+            patient.Therapists.Remove(item);
+        }
+    }
+
+    public void OnAdd(Therapist item)
+    {
+    }
+}

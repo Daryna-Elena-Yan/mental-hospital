d5d084e72ac318a5bfb0ad3065a8bc7309f13ac8
Yan-Pavel Hantsavich <s26822@pjwstk.edu.pl>
2024-10-23 12:57:33 +0200

    Added serializing class

---
+++ Mental_Hospital/Mental_Hospital/Services/FileService.cs
@@ -0,0 +1,17 @@
+﻿using System.Text.Json;
+namespace Mental_Hospital.Services;
+
+public class FileService
+{
+    static void Serialize<T>(List<T> objects)
+    {
+        string jsonString = JsonSerializer.Serialize(objects);
+        File.WriteAllText($"{typeof(T).Name}.json", jsonString);
+    }
+    static List<T>? Deserialize<T>()
+    {
+        string json = File.ReadAllText($"{typeof(T).Name}.json");
+        return JsonSerializer.Deserialize<List<T>>(json);
+    }
+}
+

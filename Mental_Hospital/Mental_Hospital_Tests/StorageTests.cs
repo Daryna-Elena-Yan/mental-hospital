33be7f0cd7ae2a631ece0f59e4a5e283c3684afa
s26874 <s26874@pjwstk.edu.pl>
2024-10-23 21:45:10 +0200

    Improved DiagnosisFactory.cs and created Unit Test for checking if all types of Disorders can be created and stored correctly

--- Mental_Hospital/Mental_Hospital_Tests/StorageTests.cs
+++ Mental_Hospital/Mental_Hospital_Tests/StorageTests.cs
@@ -1,6 +1,8 @@
 using Mental_Hospital;
 using Mental_Hospital.Factories;
 using Mental_Hospital.Models;
+using Mental_Hospital.Models.Light;
+using Mental_Hospital.Models.Severe;
 using Mental_Hospital.Storages;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -53,16 +55,17 @@ public class Tests
       Assert.That(patient.Diagnoses.Count, Is.EqualTo(1));
       Assert.That(diagnosis.Patient, !Is.Null);
       
-      
       _personStorage.Delete(patient);
       
       Assert.That(_personStorage.Count, Is.EqualTo(0));
       Assert.That(_diagnosisStorage.Count, Is.EqualTo(0));
-      
     }
     
+    
+    
+    
     [Test]
-    public void AddNewDiagnosisInPatientsListOfDiagnosisesTest()
+    public void AddNewDiagnosisTest()
     {
         var patient =  _personFactory.CreateNewPatient("Charles", "Leclerc", DateTime.Now,
             "Baker Street, 221B", "Depression", null);
@@ -74,4 +77,51 @@ public class Tests
         Assert.That(patient.Diagnoses.Where(x => x == diagnosis).Count, Is.EqualTo(1));
         
     }
+    
+    [Test]
+    public void DeleteDiagnosisTest()
+    {
+        var patient =  _personFactory.CreateNewPatient("Charles", "Leclerc", DateTime.Now,
+            "Baker Street, 221B", "Depression", null);
+        var diagnosis = _diagnosisFactory.CreateNewLightAnxiety
+            (patient, "anexity", "aaaaa", new string[0], DateTime.Now, null);
+        
+        _diagnosisStorage.Delete(diagnosis);
+        
+        Assert.That(_diagnosisStorage.Count, Is.EqualTo(0));
+        Assert.That(patient.Diagnoses.Where(x => x == diagnosis).Count, Is.EqualTo(0));
+        
+    }
+    
+    
+    
+    [Test]
+    public void AllDiagnosisTypeCreationTest()
+    {
+        var patient =  _personFactory.CreateNewPatient("Charles", "Leclerc", DateTime.Now,
+            "Baker Street, 221B", "Depression", null);
+        var diagnosis = _diagnosisFactory.CreateNewLightAnxiety
+            (patient, "anexity", "aaaaa", new string[0], DateTime.Now, null);
+        var diagnosis4 = _diagnosisFactory.CreateNewLightMood
+            (patient, "anexity", "aaaaa", new string[0], DateTime.Now, null);
+        var diagnosis5 = _diagnosisFactory.CreateNewLightPsychotic
+            (patient, "anexity", "aaaaa", new string[0], DateTime.Now, null);
+        var diagnosis1 = _diagnosisFactory.CreateNewSevereAnxiety
+            (patient, "anexity", "aaaaa", new string[0], DateTime.Now, null, LevelOfDanger.High, true);
+        var diagnosis2 = _diagnosisFactory.CreateNewSevereMood
+            (patient, "anexity", "aaaaa", new string[0], DateTime.Now, null, LevelOfDanger.High, true);
+        var diagnosis3 = _diagnosisFactory.CreateNewSeverePsychotic
+            (patient, "anexity", "aaaaa", new string[0], DateTime.Now, null, LevelOfDanger.High, true);
+
+        
+        Assert.That(_diagnosisStorage.FindBy(x => x is SevereMood) .Count, Is.EqualTo(1));
+        Assert.That(_diagnosisStorage.FindBy(x => x is SeverePsychotic) .Count, Is.EqualTo(1));
+        Assert.That(_diagnosisStorage.FindBy(x => x is SevereAnxiety) .Count, Is.EqualTo(1));
+        
+        Assert.That(_diagnosisStorage.FindBy(x => x is LightMood) .Count, Is.EqualTo(1));
+        Assert.That(_diagnosisStorage.FindBy(x => x is LightPsychotic) .Count, Is.EqualTo(1));
+        Assert.That(_diagnosisStorage.FindBy(x => x is LightAnxiety) .Count, Is.EqualTo(1));
+        
+    }
+    
 }

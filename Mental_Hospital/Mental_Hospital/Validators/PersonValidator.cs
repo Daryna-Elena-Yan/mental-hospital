f4a51ffacbd83133f213229d054ed5046b95dda6
Daryna Vlasiuk <s26878@pjwstk.edu.pl>
2024-11-11 01:02:37 +0100

    Finished Prescription validation tests, completed some Appointment validation tests

--- Mental_Hospital/Mental_Hospital/Validators/PersonValidator.cs
+++ Mental_Hospital/Mental_Hospital/Validators/PersonValidator.cs
@@ -7,8 +7,8 @@ public class PersonValidator : AbstractValidator<Person>
 {
     public PersonValidator()
     {
-        RuleFor(x => x.Name).MinimumLength(2).WithMessage("Name should ba at least 2 characters long.");
-        RuleFor(x => x.Surname).MinimumLength(2).WithMessage("Surname should ba at least 2 characters long.");
+        RuleFor(x => x.Name).MinimumLength(2).WithMessage("Name should be at least 2 characters long.");
+        RuleFor(x => x.Surname).MinimumLength(2).WithMessage("Surname should be at least 2 characters long.");
         RuleFor(x => x.Address).Length(10, 70).WithMessage("Address must be of length from 10 to 70 symbols.");
         RuleFor(x => x.DateOfBirth).NotNull()
             .Must(x => x != DateTime.MinValue)
